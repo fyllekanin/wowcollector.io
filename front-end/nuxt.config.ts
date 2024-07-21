00de94a6cf25805d6dd70d286b5ad71a60b94474
Philip Rutberg <philip.rutberg@postnord.com>
2024-07-21 21:04:50 +0200

    setup config

--- front-end/nuxt.config.ts
+++ front-end/nuxt.config.ts
@@ -1,5 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  runtimeConfig: {},
+  $development: {},
+  $production: {},
+  srcDir: 'src',
+  serverDir: 'server',
+  extends: ['@nuxt/ui-pro'],
+  modules: ['@nuxt/ui', '@nuxt/content'],
+  components: [
+    {
+      path: '~/components',
+      pathPrefix: false,
+    },
+  ],
+});
---
+++ front-end/src/app.config.ts
@@ -0,0 +1,9 @@
+export default defineAppConfig({
+  ui: {
+    primary: 'orange',
+    gray: 'neutral',
+    icons: {
+      dynamic: true,
+    },
+  },
+});
