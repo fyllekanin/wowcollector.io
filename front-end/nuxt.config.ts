9334cb2a8bad0c2c173ecaa30a80874dec568218
Philip Rutberg <philip.rutberg@postnord.com>
2024-07-28 14:50:09 +0200

    fix baseURL

--- front-end/nuxt.config.ts
+++ front-end/nuxt.config.ts
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
   runtimeConfig: {},
   $development: {
     runtimeConfig: {
-      baseURL: 'http://localhost:8888',
+      baseURL: 'http://127.0.0.1:8888',
     },
   },
   $production: {
