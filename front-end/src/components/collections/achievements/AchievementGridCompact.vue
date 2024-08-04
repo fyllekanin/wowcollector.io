f50b92232b690c01f521d7fc949c7cf763172fae
Philip Rutberg <philip.rutberg@postnord.com>
2024-08-04 17:09:07 +0200

    spinner?

--- front-end/src/components/collections/achievements/AchievementGridCompact.vue
+++ front-end/src/components/collections/achievements/AchievementGridCompact.vue
@@ -2,6 +2,7 @@
 import type { AchievementCategory } from '~/types';
 
 const loading = ref(false);
+const loadingCategoryId = ref<number | null>(null);
 
 const toast = useToast();
 const achievementsStore = useAchievementsStore();
@@ -18,6 +19,7 @@ const fetchAchievementCategory = async (
 
   try {
     loading.value = true;
+    loadingCategoryId.value = category.id;
     const data = await $fetch(
       `/api/character/${character.value?.region}/${character.value?.realm}/${character.value?.name}/achievements`,
       {
@@ -39,6 +41,7 @@ const fetchAchievementCategory = async (
     });
   } finally {
     loading.value = false;
+    loadingCategoryId.value = null;
   }
 };
 </script>
@@ -56,6 +59,7 @@ const fetchAchievementCategory = async (
   >
     <template #default="{ item, open }">
       <UButton
+        color="gray"
         variant="ghost"
         class="border-b border-gray-200 dark:border-gray-700"
         :ui="{ rounded: 'rounded-none', padding: { sm: 'p-3' } }"
@@ -73,19 +77,25 @@ const fetchAchievementCategory = async (
       </UButton>
     </template>
     <template #item="{ item }">
-      <UContainer
-        v-if="loading"
+      <!-- <UContainer
         class="flex flex-wrap w-full justify-center md:justify-start px-3 lg:px-3 sm:px-0 py-2 lg:py-2 sm:py-2 mx-0 gap-4"
-      >
-        <USkeleton
+      > -->
+
+      <!-- <UProgress
+        v-if="loading && loadingCategoryId === item.id"
+        class="w-full"
+        animation="carousel"
+      /> -->
+
+      <!-- <USkeleton
           v-for="i in 300"
           :key="i"
           class="h-8 w-8"
           :ui="{
             rounded: 'none',
           }"
-        />
-      </UContainer>
+        /> -->
+      <!-- </UContainer> -->
       <UContainer
         v-if="item.achievements?.length"
         class="flex flex-wrap w-full justify-center md:justify-start px-3 lg:px-3 sm:px-0 py-2 lg:py-2 sm:py-2 mx-0 gap-4"
