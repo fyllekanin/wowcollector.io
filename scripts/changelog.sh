95d7201fb079553ae4cb86b4d533ca219d699efc
Eric <eric.christensen94@gmail.com>
2024-08-09 15:50:10 +0200

    Remove color blue

--- scripts/changelog.sh
+++ scripts/changelog.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
-# Colors
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
-BLUE='\033[0;34m'
-NC='\033[0m' # No Color
+NC='\033[0m'
 
-# Check if the script is run from the root of the git repo
 if [ ! -d ".git" ]; then
     echo -e "${RED}Error: This script must be run from the root of the Git repository.${NC}"
     exit 1
 fi
 
-# Ask the user for the type of change
 while true; do
     echo -e "${YELLOW}What type of change is this? (1 = breaking change, 2 = feature, 3 = bug fix):${NC}"
     read change_type
@@ -37,14 +33,11 @@ while true; do
     esac
 done
 
-# Ask the user for the description of the change
 echo -e "${YELLOW}Please describe the change you made:${NC}"
 read description
 
-# Get the current Unix timestamp
 timestamp=$(date +%s)
 
-# Create the JSON content
 json_content=$(cat <<EOF
 {
     "type": "$type",
@@ -54,11 +47,8 @@ json_content=$(cat <<EOF
 EOF
 )
 
-# Define the filename
 filename="${timestamp}.json"
 
-# Save the JSON content to the file
 echo "$json_content" > "changelog/current/$filename"
 
-# Output the filename
 echo -e "${GREEN}Change log saved to $filename${NC}"
