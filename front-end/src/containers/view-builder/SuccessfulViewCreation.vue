01e9134418b490d951ff1dc14cbcd529bb7bb3a8
Philip Rutberg <philip.rutberg@postnord.com>
2024-08-31 21:43:52 +0200

    pop confetti again, easter egg

--- front-end/src/containers/view-builder/SuccessfulViewCreation.vue
+++ front-end/src/containers/view-builder/SuccessfulViewCreation.vue
@@ -17,6 +17,14 @@ const toast = useToast();
 const characterStore = useCharacterStore();
 const { character } = storeToRefs(characterStore);
 
+const visible = ref(true);
+
+const explode = async () => {
+  visible.value = false;
+  await nextTick();
+  visible.value = true;
+};
+
 function copyLink() {
   clipboard.copy(viewId.value);
   toast.add({
@@ -45,9 +53,12 @@ onBeforeRouteLeave(() => {
 
 <template>
   <div class="flex flex-col items-center justify-center h-full w-full">
-    <ConfettiExplosion />
+    <ConfettiExplosion v-if="visible" :stageHeight="1300" />
     <div class="flex flex-col items-center justify-center mt-20 gap-5 w-full">
-      <h2 class="text-2xl font-bold text-center">Congratulations! 🎉</h2>
+      <div class="flex gap-2 items-center">
+        <h2 class="text-2xl font-bold text-center">Congratulations!</h2>
+        <span class="cursor-pointer select-none" @click="explode"> 🎉 </span>
+      </div>
       <h3 class="text-lg font-semibold text-center">
         You have successfully created a new view!
       </h3>
@@ -77,15 +88,17 @@ onBeforeRouteLeave(() => {
       <UButton to="/" icon="heroicons-outline:home" color="gray">
         Go back home
       </UButton>
-      or
-      <UButton
-        v-if="character"
-        :to="`/collections/${character.region}/${character.realm}/${character.name}/${to}?viewId=${viewId}`"
-        icon="heroicons-outline:eye"
-        color="gray"
-      >
-        Go to view
-      </UButton>
+      <div v-if="character" class="flex gap-2 items-center mt-8">
+        or
+        <UButton
+          v-if="character"
+          :to="`/collections/${character.region}/${character.realm}/${character.name}/${to}?viewId=${viewId}`"
+          icon="heroicons-outline:eye"
+          color="gray"
+        >
+          Go to view
+        </UButton>
+      </div>
     </div>
     <ConfettiExplosion />
   </div>
