6cc79644c854b96d83ecd96f3827f9c53c64c60f
Philip Rutberg <philip.rutberg@postnord.com>
2024-09-01 16:06:57 +0200

    faction and icons

--- front-end/src/stores/view-builder.mounts.ts
+++ front-end/src/stores/view-builder.mounts.ts
@@ -32,10 +32,32 @@ export const useMountViewBuilderStore = defineStore('mount-view-builder', {
     _settings: {
       showBorders: true,
       showMountTooltips: true,
+      showFaction: {
+        label: 'Both',
+        value: 'both',
+        avatar: {
+          src: 'https://cdn.discordapp.com/attachments/1161263238554599464/1279760555237838899/12d4f5a73e9c1b830c95229ac396a449.png?ex=66d59d65&is=66d44be5&hm=84d99f37f7c08df863528175f78e6853a19b5c6a48f31b1e7e0684708856139a&',
+        },
+      } as {
+        label: string;
+        value: string;
+        avatar: {
+          src: string;
+        };
+      },
     },
     successfulCreation: false,
   }),
   getters: {
+    mounts(state): MountInformation[] {
+      let mounts = state._mounts || [];
+
+      if (state._settings.showFaction.value === 'both') {
+        return mounts;
+      }
+
+      return mounts;
+    },
     flatMounts(state): MountInformation[] {
       return state._mounts || [];
     },
