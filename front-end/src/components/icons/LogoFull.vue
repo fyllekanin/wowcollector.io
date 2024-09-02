924fea61f69080d5967e22d35d22078b8b75ecfb
Philip Rutberg <philip.rutberg@postnord.com>
2024-09-02 12:32:03 +0200

    dynamic color mode logo

--- front-end/src/components/icons/LogoFull.vue
+++ front-end/src/components/icons/LogoFull.vue
@@ -1,9 +1,19 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const colorMode = useColorMode();
+const isDark = computed(() => colorMode.value === 'dark');
+</script>
 
 <template>
   <img
+    v-if="isDark"
     src="../../assets/img/logo.webp"
     alt="Logo"
     style="height: 32px; width: auto"
+  />
+  <img
+    v-else
+    src="../../assets/img/logo_black.webp"
+    alt="Logo"
+    style="height: 22px; width: auto"
   />
 </template>
